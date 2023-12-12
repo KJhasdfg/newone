3c560eabe3b18a7236887b9d3165a5b0018c6941
KJhasdfg <alexdahal12@gmail.com>
2023-12-12 23:30:52 +0545

    small change

--- sentiment_analysis_project/urls.py
+++ sentiment_analysis_project/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
      path('', LandingPage, name='landing_page'),
     path('signin/', signin, name='signin'),
     path('signup/', signup, name='signup'),
-     path('logged_out/', logged_out, name='logged_out'),
+    path('logged_out/', logged_out, name='logged_out'),
     path('sentiment/', include('sentiment_analysis_app.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
