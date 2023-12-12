4a9f7f03ef7fa82156152bed10f91abfc922cdaa
KJhasdfg <alexdahal12@gmail.com>
2023-12-12 21:50:22 +0545

    third

--- sentiment_analysis_app/views.py
+++ sentiment_analysis_app/views.py
@@ -4,34 +4,6 @@ from .forms import AnalyzedTextForm
 from .models import AnalyzedText
 from django.http import HttpResponse
 
-
-def analyze_text(request):
-    if request.method == 'POST':
-        form = AnalyzedTextForm(request.POST)
-        if form.is_valid():
-            text = form.cleaned_data['text']
-            AnalyzedText.objects.create(text=text)
-            return redirect('result')
-    else:
-        form = AnalyzedTextForm()
-
-    return render(request, 'analyze_text.html', {'form': form})
-
-def result(request):
-    texts = AnalyzedText.objects.all()
-    data = []
-    for text in texts:
-        if text.sentiment == 0:
-            data.append({"sentimentDescription": "neutral", "sentiment": text.text, "score": text.sentiment})
-        elif text.sentiment < 0:
-            data.append({"sentimentDescription": "Sad", "sentiment": text.text, "score": text.sentiment})
-        elif 0 < text.sentiment <= -0.5:
-            data.append({"sentimentDescription": "depressed", "sentiment": text.text, "score": text.sentiment})
-        else:
-            data.append({"sentimentDescription": "happy", "sentiment": text.text, "score": text.sentiment})
-
-    return render(request, 'result.html', {'texts': data})
-
 def clear_history(request):
     return render(request, 'result.html', {'texts': []})
 
@@ -64,7 +36,7 @@ def result(request):
             data.append({"sentimentDescription": "neutral", "sentiment": text.text, "score": text.sentiment})
         elif text.sentiment < 0:
             data.append({"sentimentDescription": "Sad", "sentiment": text.text, "score": text.sentiment})
-        elif 0 < text.sentiment <= -0.5:
+        elif 0 < text.sentiment <= -2:
             data.append({"sentimentDescription": "depressed", "sentiment": text.text, "score": text.sentiment})
         else:
             data.append({"sentimentDescription": "happy", "sentiment": text.text, "score": text.sentiment})
